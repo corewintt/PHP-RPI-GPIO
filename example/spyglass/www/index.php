ab5d15e2612226e418bce43c8151a0d864605337
corewintt <p1dl0@yandex.ru>
2014-10-25 15:35:38 +0400

    Add new style

--- example/spyglass/www/index.php
+++ example/spyglass/www/index.php
@@ -2,7 +2,9 @@
 <html>
 <head>
 	<link rel="stylesheet" href="css/reset.css" type="text/css">
-	<script type="text/javascript" async="" src="js/jquery-2.1.1.min.js"></script>
+    <link rel="stylesheet" href="css/styler.css" type="text/css">
+
+    <script type="text/javascript" async="" src="js/jquery-2.1.1.min.js"></script>
 
 	<title>SpyGlass V.1</title>
 </head>
