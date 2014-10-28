9b9b79fbffa7481122966260f2f39d74e330904d
Vasily <v.popkov@fabrikant.ru>
2014-10-28 08:20:07 +0300

    First Example

--- example/spyglass/www/index.php
+++ example/spyglass/www/index.php
@@ -1,12 +1,46 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-	<link rel="stylesheet" href="css/reset.css" type="text/css">
-	<script type="text/javascript" async="" src="js/jquery-2.1.1.min.js"></script>
-
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>SpyGlass V.1</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<div id="wrapper">
+	<div id="top_panel">
+		<div id="view">
+
+
+		</div>
+
+		<div id="control">
+
+			<div id="foto">
+			</div>
+			<div id="video">
+			</div>
+			<div id="sekyndomer">
+			</div>
+			<div id="big">
+			</div>
+			<div id="playlist">
+			</div>
+
+		</div>
+		<div class="clearFix"></div>
+
+	</div>
+
+
+	<div id="playlist_big">
+	</div>
+
+	<div id="playlist_big_razvorot">
+	</div>
+
+
+</div>
 
 </body>
 </html>
