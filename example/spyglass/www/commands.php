81444390ae4ebd08e312808c1cb6c48aa6d1b049
corewintt <p1dl0@yandex.ru>
2014-12-05 22:23:17 +0400

    Add new style

--- example/spyglass/www/commands.php
+++ example/spyglass/www/commands.php
@@ -1,6 +1,11 @@
 <?php
 include '../../../lib/GPIO.php';
-# end up 
+# end up
+
+switch($_GET['command'])
+{
+    case 'up':
+
 $pin2 = GPIO::get(2);
 $pin2->setMode(GPIO::MODE_OUT);
 
@@ -17,3 +22,24 @@ $pin3->writeDigital(true);
 usleep(100000);
 $pin2->writeDigital(0);
 $pin3->writeDigital(0);
+        break;
+    case 'down':
+
+$pin2 = GPIO::get(2);
+$pin2->setMode(GPIO::MODE_OUT);
+
+#end down
+$pin3 = GPIO::get(3);
+$pin3->setMode(GPIO::MODE_OUT);
+
+
+$pin2->writeDigital(0);
+$pin3->writeDigital(0);
+
+$pin2->writeDigital(true);
+
+usleep(100000);
+$pin2->writeDigital(0);
+$pin3->writeDigital(0);
+        break;
+}
--- example/spyglass/www/index.php
+++ example/spyglass/www/index.php
@@ -1,64 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
     <link rel="stylesheet" href="css/styler.css" type="text/css">
 
     <script type="text/javascript" src="js/jquery.js"></script>
 
-	<title>SpyGlass V.1</title>
+    <title>SpyGlass V.1</title>
+    <style>
+        .ajax_command {
+            display: block;
+            position: absolute;
+            width: 40px;
+            height: 40px;
+        }
+
+    </style>
 </head>
 
 <body style="background:darkslategrey">
 
-<div>
-    <img src="/images/control_osnova.png">
-    
-    
+<div style="width: 1007px;margin: 10px auto">
+
+    <div id="control_area">
+
+
+        <div style="position: relative;height: 276px;width: 688px;margin: 0px auto">
+            <a href="/commands.php?command=up" class="ajax_command"
+               style="left: 117px;top:75px"></a>
+            <a href="/commands.php?command=down" class="ajax_command"
+               style="left: 117px;top:162px"></a>
+            <a href="/commands.php?command=left" class="ajax_command"
+               style="left: 72px;top: 118px;"></a>
+            <a href="/commands.php?command=right" class="ajax_command"
+               style="left: 162px;top: 118px;"></a>
+            <img src="/images/control_osnova.png">
+        </div>
+        <div class="clearFix"></div>
+    </div>
+    <div style="position: relative;width: 1007px;margin: 0px auto" >
+        <img id="streamimage" class="x-rotated-180" src="/stream/" height="714px"
+             style="position: absolute;left: 27px;top:5px"/>
+        <img src="images/playlist_big-razvorot.png" style="position: absolute;"/>
+    </div>
 </div>
 
-<div style="position: relative;width: 1007px;margin: 0px auto">
-    <img id="streamimage" class="x-rotated-180" src="/stream/" height="700px" style="position: absolute;left: 37px;top:10px"/>
-    <img src="images/playlist_big-razvorot.png" style="position: absolute;" />
-</div>
 
-
-<p style="text-align:center">
-
-</p>
-
-<table width="100%">
-    <tr>
-        <td style="text-align:center">
-            <a href="/commands.php" class="ajax_command">up</a>
-        </td>
-        <td style="text-align:center">
-            <a href="/commands.php" class="ajax_command">down</a>
-        </td>
-    </tr>
-    <table>
-
-
-        <script type="text/javascript">
-
-            $(document).ready(function(){
-                $('.ajax_command').click(function(){
-                    var obj = jQuery(this);
-                    var url = obj.attr('href');
-                    console.log(obj);
-                    $.ajax({
-                        url: url,
-                        type: "POST",
-                        data: data
-
-
-                    });
-
-                    return false;
-                })
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.ajax_command').click(function () {
+            var obj = jQuery(this);
+            var url = obj.attr('href');
+            console.log(obj);
+            $.ajax({
+                url: url,
+                type: "POST"
             });
-
-
-        </script>
+            return false;
+        })
+    });
+</script>
 </body>
 </html>
